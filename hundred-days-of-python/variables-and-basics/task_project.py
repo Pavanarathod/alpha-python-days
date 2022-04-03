397ad4916d00cc901e5c13be0e5b730fc9636d75
Pavanarathod <edrocullen13@gmail.com>
2022-04-03 10:07:41 +0530

    task

---
+++ hundred-days-of-python/variables-and-basics/task_project.py
@@ -0,0 +1,6 @@
+print("Welcome to the project..!")
+
+city = str(input("Entter the city name you grown up: "))
+favorite_band = str(input("Enter you favorite band name: "))
+
+print(f"your band name is {city} {favorite_band}")
