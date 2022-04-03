a8a2d74f6a2d5555b63e5887427fbd622995e339
Pavanarathod <edrocullen13@gmail.com>
2022-04-03 08:53:58 +0530

    testing

---
+++ hundred-days-of-python/variables-and-basics/variables.py
@@ -0,0 +1,7 @@
+first_name = "Pavan"
+last_name = "Pattinson"
+import datetime
+
+def full_name(fname, lname):
+    return f"your full name is {fname} {lname}"
+print(full_name(first_name, last_name))
